0abb4c2b4ed93e0050882a9a265d5985d1d51fb8
Lerit Nuksawn <lerit.ns@gmail.com>
2019-07-02 10:56:49 +0700

    Add new feature to get data from every .rdq files in a folder - Extract function that use to restore data

--- rdqdump.py
+++ rdqdump.py
@@ -23,74 +23,13 @@ from argparse import ArgumentParser
 import json
 import yaml
 from helpers.rabbitmq import RabbitMQ
+from helpers.hex import hexdump, convert_hex
 
 
 def read_chunk(data, start, end):
     data.seek(int(start))
     readdata = data.read(end)
     return readdata
-
-
-# Hex dump code from:
-# Author: Boris Mazic
-# Date: 04.06.2012
-# package rfid.libnfc.hexdump
-def hexbytes(xs, group_size=1, byte_separator=' ', group_separator=' '):
-    def ordc(c):
-        return ord(c) if isinstance(c,str) else c
-
-    if len(xs) <= group_size:
-        s = byte_separator.join('%02X' % (ordc(x)) for x in xs)
-    else:
-        r = len(xs) % group_size
-        s = group_separator.join(
-            [byte_separator.join('%02X' % (ordc(x)) for x in group) for group in zip(*[iter(xs)] * group_size)]
-        )
-        if r > 0:
-            s += group_separator + byte_separator.join(['%02X' % (ordc(x)) for x in xs[-r:]])
-    return s.lower()
-
-
-def hexprint(xs):
-    def chrc(c):
-        return c if isinstance(c, str) else chr(c)
-
-    def ordc(c):
-        return ord(c) if isinstance(c, str) else c
-
-    def isprint(c):
-        return ordc(c) in range(32,127) if isinstance(c, str) else c > 31
-
-    return ''.join([chrc(x) if isprint(x) else '.' for x in xs])
-
-
-def hexdump(xs, group_size=4, byte_separator=' ', group_separator='-', printable_separator='  ', address=0, address_format='%04X', line_size=16):
-    if address is None:
-        s = hexbytes(xs, group_size, byte_separator, group_separator)
-        if printable_separator:
-            s += printable_separator + hexprint(xs)
-    else:
-        r = len(xs) % line_size
-        s = ''
-        bytes_len = 0
-        for offset in range(0, len(xs) - r, line_size):
-            chunk = xs[offset:offset + line_size]
-            bytes = hexbytes(chunk, group_size, byte_separator, group_separator)
-            s += (address_format + ': %s%s\n') % (address + offset, bytes, printable_separator + hexprint(chunk) if printable_separator else '')
-            bytes_len = len(bytes)
-
-        if r > 0:
-            offset = len(xs) - r
-            chunk = xs[offset:offset + r]
-            bytes = hexbytes(chunk, group_size, byte_separator, group_separator)
-            bytes = bytes + ' ' * (bytes_len - len(bytes))
-            s += (address_format + ': %s%s\n') % (address + offset, bytes, printable_separator + hexprint(chunk) if printable_separator else '')
-    return s
-
-
-def convert_hex(string):
-    return ''.join([hex(character)[2:].upper().zfill(2) \
-                    for character in string])
 
 
 def save_data(output_file, queue_name, queue_message, file_location='output'):
@@ -132,73 +71,30 @@ def extract_unescaped_content(src, data, hex_bytes, chunk_size, data_length_byte
 
     return (data, unescaped_data)
 
-if __name__ == '__main__':
-    # search a rabbit mq rdq file for
-    # potential amqp records:
-    # by finding: 395f316c000000016d0000 , parsing the next 2 bytes as
-    # record length and outputing the record to stdout
-    #
-    parser = ArgumentParser(description="Process RabbitMQ persistent file storage (.rdq) and output to file or another RabbitMQ")
-    parser.add_argument("-b", dest='bytes', default=16, type=int, help="number of bytes to show per line")
-    parser.add_argument("-s", dest='start', default=0, type=int, help="starting byte")
-    parser.add_argument("-l", dest='length', default=16, type=int, help="length in bytes to dump")
-    parser.add_argument("-r", dest='chunk', default=1024, type=int, help="length in bytes to read at a time")
-    parser.add_argument("-f", dest='input', default='', help="input: filename")
-    parser.add_argument("-t", dest='text', default='', help="text string to search for")
 
-    # output related arguments
-    parser.add_argument("-o", dest='output', help="output: filename")
-    parser.add_argument("-q", dest='queue', help="config containing output queue credential")
+def restore_rdq_data(
+        file_path,
+        queue_name_indicator_hex,
+        queue_message_indicator_hex,
+        chunk,
+        message_limit,
+        output_file,
+        queue,
+        start_bytes=0,
+        debug=False,
+        dev_debug=False,
+        zero=False
+        ):
+    src, chunk_size = read_file(file_path, chunk)
 
-    # this hex value worked for me, might work for you
-    # to delimit the entries in a rabbitmq .rdq file
-    parser.add_argument("-x", "--msg-hex", dest='hex', default="395f316c000000016d0000", help="hex string to search for queue message")
-    parser.add_argument("--hex_queue", dest='hex_queue', default="65786368616E67656D000000006C000000016D000000,000865786368616E67656D000000", help="hex string to search for queue name")
-    parser.add_argument("-c", dest='count', default=1, type=int, help="count of hits to find before stopping (0 for don't stop)")
-
-    # debug related arguments
-    parser.add_argument("--dev", action='store_true', dest='dev_debug', default=False, help="Print debug message related to development process")
-    parser.add_argument("-d", "--debug", action='store_true', dest='debug', default=False, help="turn on debugging output")
-    parser.add_argument("-z", "--zero", action='store_true', dest='zero', default=False, help="when printing output, count from zero rather than position hit was found")
-
-    options = parser.parse_args()
-    dev_debug = options.dev_debug
-
-    if os.path.exists(options.input):
-        src = open(options.input, 'rb')
-        # if the file is smaller than our chunksize, reset.
-        chunk_size = min(os.path.getsize(options.input), options.chunk)
-    else:
-        sys.stderr.write(options.input)
-        sys.stderr.write("No input file specified\n")
-        sys.exit()
-
-    if options.output is not None:
-        output_file = options.output
-        output = True
-    else:
-        output = False
-
-    if options.queue is not None:
-        with open(f'config/{options.queue}') as stream:
-            queue_config = yaml.safe_load(stream)['queue']
-        result_queue_connection = RabbitMQ(queue_config['user'], queue_config['password'], queue_config['host'], queue_config['port'])
-        result_queue_connection.connect_to_queue()
-        queue = True
-    else:
-        queue = False
-
-    data = read_chunk(src, options.start, chunk_size)
-    if options.debug:
+    if debug:
         print("[*] position: %d" % (src.tell()))
-    count = 0
 
+    count = 0
     queue_name = None
     queue_message = None
 
-    queue_name_indicator_hex = options.hex_queue.split(',')
-    queue_message_indicator_hex = options.hex
-
+    data = read_chunk(src, start_bytes, chunk_size)
     while data:
         if dev_debug:
             print("Data:")
@@ -218,7 +114,7 @@ if __name__ == '__main__':
                     hex_bytes=hex_bytes,
                     chunk_size=chunk_size,
                     data_length_byte_size=1,
-                    zero=options.zero
+                    zero=zero
                 )
 
         if len(queue_message_indicator_hex) > 0 and queue_message_indicator_hex.upper() in convert_hex(data):
@@ -230,7 +126,7 @@ if __name__ == '__main__':
                 hex_bytes=queue_message_indicator_hex,
                 chunk_size=chunk_size,
                 data_length_byte_size=2,
-                zero=options.zero
+                zero=zero
             )
             count += 1
 
@@ -238,15 +134,15 @@ if __name__ == '__main__':
             if queue_name is None:
                 raise Exception("ERROR [3], QUEUE NAME NOT FOUND FOR QUEUE MESSAGE")
             
-            if (not queue) and (not output):
+            if (not queue) and (not output_file):
                 print(f"queue_name:\n{queue_name}\n")
                 print(f"queue_message:\n{queue_message}")
                 print("\n\n=====================================================================================\n\n")
             else:
                 if queue:
-                    result_queue_connection.create_queue(queue_name)
-                    result_queue_connection.publish_to_queue(queue_name, queue_message)
-                if output:
+                    queue.create_queue(queue_name)
+                    queue.publish_to_queue(queue_name, queue_message)
+                if output_file:
                     save_data(output_file, queue_name, queue_message)
             queue_name = None
             queue_message = None
@@ -254,11 +150,110 @@ if __name__ == '__main__':
         if count % 1000 == 0:
             print(f"Processed {count} messages.")
 
-        if options.count != 0 and options.count <= count:
+        if message_limit != 0 and message_limit <= count:
             sys.exit()
         else:
             data = read_chunk(src,src.tell(), chunk_size)
-            if options.debug:
+            if debug:
                 print("[*] position: %d" % (src.tell()))
 
-    print(f"\nFinish processing {count} messages from {options.input}.")
+    print(f"\nFinish processing {count} messages from {file_path}.")
+
+
+def read_file(file_path, chunk):
+    if os.path.exists(file_path):
+        src = open(file_path, 'rb')
+        # if the file is smaller than our chunksize, reset.
+        chunk_size = min(os.path.getsize(file_path), chunk)
+    else:
+        sys.stderr.write(f"Cannot find the specified file '{file_path}'\n")
+        sys.exit()
+    return src, chunk_size
+
+
+if __name__ == '__main__':
+    # search a rabbit mq rdq file for
+    # potential amqp records:
+    # by finding: 395f316c000000016d0000 , parsing the next 2 bytes as
+    # record length and outputing the record to stdout
+    #
+    parser = ArgumentParser(description="Process RabbitMQ persistent file storage (.rdq) and output to file or another RabbitMQ")
+    parser.add_argument("-b", dest='bytes', default=16, type=int, help="number of bytes to show per line")
+    parser.add_argument("-s", dest='start', default=0, type=int, help="starting byte")
+    parser.add_argument("-l", dest='length', default=16, type=int, help="length in bytes to dump")
+    parser.add_argument("-r", dest='chunk', default=1024, type=int, help="length in bytes to read at a time")
+    parser.add_argument("-i", dest='input', default=None, help="input: filename")
+    parser.add_argument("-f", "--folder", dest='folder', default=None, help="input: folder path")
+    parser.add_argument("-t", dest='text', default='', help="text string to search for")
+
+    # output related arguments
+    parser.add_argument("-o", dest='output', help="output: filename")
+    parser.add_argument("-q", dest='queue', help="config containing output queue credential")
+
+    # this hex value worked for me, might work for you
+    # to delimit the entries in a rabbitmq .rdq file
+    parser.add_argument("-x", "--msg-hex", dest='hex', default="395f316c000000016d0000", help="hex string to search for queue message")
+    parser.add_argument("--hex_queue", dest='hex_queue', default="65786368616E67656D000000006C000000016D000000,000865786368616E67656D000000", help="hex string to search for queue name")
+    parser.add_argument("-c", dest='count', default=1, type=int, help="count of hits to find before stopping (0 for don't stop)")
+
+    # debug related arguments
+    parser.add_argument("--dev", action='store_true', dest='dev_debug', default=False, help="Print debug message related to development process")
+    parser.add_argument("-d", "--debug", action='store_true', dest='debug', default=False, help="turn on debugging output")
+    parser.add_argument("-z", "--zero", action='store_true', dest='zero', default=False, help="when printing output, count from zero rather than position hit was found")
+
+    options = parser.parse_args()
+
+    if options.input is None and options.folder is None:
+        sys.stderr.write("No input file (-i) or folder (-f) specified\n")
+        sys.exit()
+
+    output_file = None
+    if options.output is not None:
+        output_file = options.output
+
+    queue = None
+    if options.queue is not None:
+        with open(f'config/{options.queue}') as stream:
+            queue_config = yaml.safe_load(stream)['queue']
+        queue = RabbitMQ(queue_config['user'], queue_config['password'], queue_config['host'], queue_config['port'])
+        queue.connect_to_queue()
+
+    queue_name_indicator_hex = options.hex_queue.split(',')
+    queue_message_indicator_hex = options.hex
+
+    if options.input:
+        file_path = options.input
+        restore_rdq_data(
+            file_path=file_path,
+            queue_name_indicator_hex=queue_name_indicator_hex,
+            queue_message_indicator_hex=queue_message_indicator_hex,
+            chunk=options.chunk,
+            message_limit=options.count,
+            output_file=output_file,
+            queue=queue,
+            start_bytes=options.start,
+            debug=options.debug,
+            dev_debug=options.dev_debug
+        )
+
+    if options.folder:
+        folder_path = options.folder.rstrip('/')
+        if os.path.exists(folder_path):
+            rdq_list = list(filter(lambda file_name: file_name.endswith('.rdq'), os.listdir(folder_path)))
+            for rdq_file in rdq_list:
+                full_path = f"{folder_path}/{rdq_file}"
+                restore_rdq_data(
+                    file_path=full_path,
+                    queue_name_indicator_hex=queue_name_indicator_hex,
+                    queue_message_indicator_hex=queue_message_indicator_hex,
+                    chunk=options.chunk,
+                    message_limit=options.count,
+                    output_file=output_file,
+                    queue=queue,
+                    start_bytes=0,
+                    debug=options.debug,
+                    dev_debug=options.dev_debug
+                )
+            
+        else:
+            sys.stderr.write(f"Cannot find the specified folder '{folder_path}'\n")
